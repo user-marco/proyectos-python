6e1a9d0bf23e727e26241e8685a359545d297eb4
Marco Antonio Chavez Salas <marco.chavez.salas.2010@gmail.com>
2026-05-18 22:12:55 -0500

    Add files via upload

---
+++ Clase 2.py
@@ -0,0 +1,33 @@
+#Hola Mundo
+script = "Hola a todos"
+print(script)
+"""
+This is my first comment in Python
+"""
+#Excersices with operators
+'Adition'
+a = 72
+b = 28
+print("The sum of a and b is: " + str(a + b))
+
+'Subtraction'
+year = 2026
+borned = 2010
+print("Currently I have " + str(year - borned) + " years old")
+
+'Multiplication'
+price = 10
+cant = 7
+print("The total price is:" + str(price*cant))
+
+'Division'
+total = 70
+people = 2
+print("The cost per person is:" + str(total // people))
+
+'Exponentiation'
+base = 2
+exponent = 8
+print("The result of " + str(base) + " raised to the power of " + str(exponent) + " is " + str(base**exponent))
+
+
